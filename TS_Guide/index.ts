58ff6485069d8afdb36598c93d0b4b69ab34ff1d
Sloan-Crawford <sloan.crawford@gmail.com>
2022-08-08 17:07:47 -0400

    Changed config settings and set up debugger

--- TS_Guide/index.ts
+++
@@ -1,2 +0,0 @@
-// set the type of age variable to number and initialize it to 20:
-let age: number = 20;
---
+++ TS_Guide/src/index.ts
@@ -0,0 +1,6 @@
+// set the type of age variable to number and initialize it to 20:
+let age: number = 20; // this is called annotating the variable
+if (age < 50)
+ age += 10;
+
+console.log(age);
